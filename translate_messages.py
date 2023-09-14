4198a32129d958398034a89d70ae43d284ea9657
Matheus Ricardo Oliveira Lima <96189591+1mxthxs@users.noreply.github.com>
2023-09-14 11:34:54 -0400

    Django VestIFAM

---
+++ translate_messages.py
@@ -0,0 +1,41 @@
+from googletrans import Translator
+import polib
+import os
+from django.core.management import call_command
+
+call_command('makemessages', '-l', 'br', '-i', 'venv/*')
+call_command('makemessages', '-l', 'en', '-i', 'venv/*')
+
+po_file = polib.pofile('locale/en/LC_MESSAGES/django.po')
+
+translator = Translator()
+
+for entry in po_file:
+    if not entry.msgstr and not entry.fuzzy: 
+        original_text = entry.msgid
+        try:
+            translated_text = translator.translate(original_text, src='pt', dest='en').text
+            entry.msgstr = translated_text
+            entry.fuzzy = False
+        except Exception as e:
+            print(f"Erro ao traduzir: {e}")
+
+po_file.save('locale/en/LC_MESSAGES/django.po')
+
+po_file = polib.pofile('locale/br/LC_MESSAGES/django.po')
+translator = Translator()
+
+for entry in po_file:
+    if not entry.msgstr and not entry.fuzzy: 
+        original_text = entry.msgid
+        try:
+            translated_text = translator.translate(original_text, src='en', dest='pt').text
+            entry.msgstr = translated_text
+            entry.fuzzy = False
+        except Exception as e:
+            print(f"Erro ao traduzir: {e}")
+
+po_file.save('locale/br/LC_MESSAGES/django.po')
+
+os.system("python manage.py compilemessages")
+os.system("python manage.py runserver")
--- website/urls.py
+++ website/urls.py
@@ -2,12 +2,19 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
+from .language import set_language 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('vest_ifam.urls'))
 ]
 
+urlpatterns = [
+    *i18n_patterns(*urlpatterns, prefix_default_language=False),
+    path("set_language/<str:language>", set_language, name="set-language"),
+    ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # Adicionar Isto
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Adicionar Isto
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
